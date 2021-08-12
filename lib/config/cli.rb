33c1f1dc13346c8362eab8d9cc170a3ced17fbb0
joey <doode1524@gmail.com>
2021-08-12 19:24:11 -0400

    parse by pipe sort 2

--- lib/config/cli.rb
+++ lib/config/cli.rb
@@ -8,7 +8,8 @@ class Cli
         # parse_by_space_sort_1
         # parse_by_space_sort_2
         # parse_by_space_sort_3
-        parse_by_pipe_sort_1
+        # parse_by_pipe_sort_1
+        parse_by_pipe_sort_2
     end
 
     def parse_by_comma_sort_1
@@ -118,6 +119,23 @@ class Cli
         reorder = parsed_input_array.each {|e| [e.slice!(2), e.push(e.slice!(3))]}
         formatted_data = reorder.each {|e| e[3] = e[3].split("-").join("/")}
         sorted_data = formatted_data.sort_by {|e| [e[2], e[0]] }
+        parsed_data = sorted_data.map {|e| e.join(" ")}
+        output_data = parsed_data.join("\n")
+        puts "---------------------------------------------"
+        puts "Output 1  (parse by pipe)"
+        puts ""
+        puts output_data
+        puts "---------------------------------------------"
+    end
+
+    def parse_by_pipe_sort_2
+        file = File.open("lib/config/pipe.txt")
+        file_input = file.read 
+        parsed_input = file_input.split("| ").join(" ").split("\n")
+        parsed_input_array = parsed_input.map {|i| i.split(" ")}
+        reorder = parsed_input_array.each {|e| [e.slice!(2), e.push(e.slice!(3))]}
+        formatted_data = reorder.each {|e| e[3] = e[3].split("-").join("/")}
+        sorted_data = formatted_data.sort_by {|e| [e[3].split("/")[2].to_i, e[0]] }
         parsed_data = sorted_data.map {|e| e.join(" ")}
         output_data = parsed_data.join("\n")
         puts "---------------------------------------------"
