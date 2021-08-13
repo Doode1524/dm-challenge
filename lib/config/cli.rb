d75b4d170c2d09b84794b6fede2816fd7157058b
joey <doode1524@gmail.com>
2021-08-12 20:00:12 -0400

    dynamic delimiter

--- lib/config/cli.rb
+++ lib/config/cli.rb
@@ -10,7 +10,61 @@ class Cli
         # parse_by_space_sort_3
         # parse_by_pipe_sort_1
         # parse_by_pipe_sort_2
-        parse_by_pipe_sort_3
+        # parse_by_pipe_sort_3
+        parse("comma", "sort_by_dob_then_last_name")
+    end
+
+    def parse(delimiter, sort_method)
+        data = parse_comma_data if delimiter === "comma"
+        data = parse_space_data if delimiter === "space"
+        data = parse_pipe_data  if delimiter === "pipe"
+
+        if sort_method === "sort_by_dob_then_last_name"
+            sort_by_dob_then_last_name(data)
+        end
+    end
+
+    def parse_comma_data
+        file = File.open("lib/config/comma.txt")
+        file_input = file.read
+        parsed_input = file_input.split(", ").join(" ").split("\n")
+        parsed_input_array = parsed_input.map {|i| i.split(" ")}
+        reorder = parsed_input_array.each {|e| e.push(e.slice!(3))}
+        return reorder 
+    end
+
+    def parse_space_data
+        file = File.open("lib/config/space.txt")
+        file_input = file.read
+        parsed_input = file_input.split("\n")
+        parsed_input_array = parsed_input.map {|i| i.split(" ")}
+        reorder = parsed_input_array.each {|e| e.slice!(2)}
+        return reorder
+    end
+
+    def parse_pipe_data
+        file = File.open("lib/config/pipe.txt")
+        file_input = file.read 
+        parsed_input = file_input.split("| ").join(" ").split("\n")
+        parsed_input_array = parsed_input.map {|i| i.split(" ")}
+        reorder = parsed_input_array.each {|e| [e.slice!(2), e.push(e.slice!(3))]}
+        return reorder
+    end
+
+    def sort_by_dob_then_last_name(data)
+        formatted_data = data.each {|e| e[3] = e[3].split("-").join("/")} 
+        sorted_data = formatted_data.sort_by {|e| [e[3].split("/")[2].to_i, e[0]] }
+        parsed_data = sorted_data.map {|e| e.join(" ")}
+        output_data = parsed_data.join("\n")
+        print_data(output_data)
+    end
+
+    def print_data(data)
+        puts "---------------------------------------------"
+        puts "Output"
+        puts ""
+        puts data
+        puts "---------------------------------------------"
     end
 
     def parse_by_comma_sort_1
