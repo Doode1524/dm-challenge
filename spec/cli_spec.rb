462064bdbadc04c3e836a80702124bd1ae1ee16a
joey <doode1524@gmail.com>
2021-08-14 16:05:16 -0400

    rspec init

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -3,9 +3,9 @@ RSpec.describe Cli do
     before do
         @cli = Cli.new
     end
+
+    it "exists" do
+        expect{Cli.new}.to_not raise_error
+    end
+
 end
-# it "works" do
-#     expect (@cli.parse("1", "1")).to output ("Kelly Sue Female 7/12/1959 Pink
-#         Abercrombie Neil Male 2/13/1943 Tan
-#         Bishop Timothy Male 4/23/1967 Yellow").to_stdout
-# end
