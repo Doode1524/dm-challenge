03328c4ea0ba634b172da84340be3709f09991fb
joey <doode1524@gmail.com>
2021-08-16 17:33:57 -0400

    parse comma test init

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -4,8 +4,24 @@ RSpec.describe Cli do
         @cli = Cli.new
     end
 
-    it "exists" do
-        expect{Cli.new}.to_not raise_error
+    context "Cli class" do
+        
+        it "exists" do
+            expect{Cli.new}.to_not raise_error
+        end
+    end
+
+    context "parse_comma_data" do
+
+        it "has a parse_comma_data method" do
+            expect{Cli.new.parse_comma_data}.to_not raise_error
+        end
+
+        it "correctly parses data" do
+            expect(Cli.new.parse_comma_data).to eq([["Abercrombie", "Neil", "Male", "2/13/1943", "Tan"],
+                ["Bishop", "Timothy", "Male", "4/23/1967", "Yellow"],
+                ["Kelly", "Sue", "Female", "7/12/1959", "Pink"]])
+        end
     end
 
 end
